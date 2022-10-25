dfe1ac2fd892d66df3936a287c685dbc0995cbe4
Emmanuele La Duca <laducaemanuele20@gmail.com>
2022-10-25 09:43:06 +0200

    exercise Deploy 2

---
+++ src/main/java/Deploy2/Controller/DeployController.java
@@ -0,0 +1,14 @@
+package Deploy2.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeployController {
+
+    @GetMapping
+    public int sum(@RequestParam int a, @RequestParam int b){
+        return a + b;
+    }
+}
